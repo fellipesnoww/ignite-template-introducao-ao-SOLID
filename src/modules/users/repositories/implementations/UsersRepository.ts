984e89e8ed8b8abc1a3c4cbed421d757f864656c
Fellipe <fellipe_emanoel@univem.edu.br>
2021-05-01 22:28:25 -0300

    Finalizando o desafio

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,43 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.users.push(user);
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    const existUser = this.users.find((user) => user.id === id);
+
+    return existUser;
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    const existUser = this.users.find((user) => user.email === email);
+
+    return existUser;
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    // eslint-disable-next-line no-param-reassign
+    receivedUser.admin = true;
+
+    // eslint-disable-next-line no-param-reassign
+    receivedUser.updated_at = new Date();
+
+    return receivedUser;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
